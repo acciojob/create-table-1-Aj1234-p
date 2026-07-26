4548c283844886c3b470e069e9146d56b519af4c
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-26 18:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    let table = document.querySelector('#sampleTable');
+	let newRow = document.createElement('tr');
+	let newRowData1 = document.createElement('td');
+	let newRowData2 = document.createElement('td');
+	newRowData1.textContent = "New Cell1"
+	newRowData2.textContent = "New Cell2"
+	newRowData1.setAttribute('value','New Cell1');
+	newRowData2.setAttribute('value','New Cell2');
+	newRow.append(newRowData1);
+	newRow.append(newRowData2);
 }
